e253210d1db8b2ea8302e817e28833d57047f020
RaphJad <21295@ecam.be>
2022-12-10 23:06:44 -0500

    add using namespace std

--- question3/investissement.cpp
+++ question3/investissement.cpp
@@ -6,6 +6,7 @@
 #include <assert.h>
 #include <numeric>
 
+using namespace std;
 // Parametres:
 //   profits: profits[i][m] = le profit d'investir m dollars dans l'entreprise i
 //   montants: parametre de sortie: montants[i] est le montant que vous voulez investir dans l'entreprise i pour maximiser les profits.
@@ -13,16 +14,16 @@ void investissement(const std::vector<std::vector<unsigned int> >& profits, std:
   assert(profits.size() > 0 && profits.front().size() > 0);
   const unsigned int nb_compagnies = profits.size();
   const unsigned int budget = profits.front().size() - 1;
-  for (std::vector<std::vector<unsigned int> >::const_iterator i = profits.begin(); i != profits.end(); i++) {
-    // Note: Lorsque l'on compile avec l'option -O3, les assertions ne
-    // sont pas incluses dans le code compile. Les boucles qui ne font
-    // rien sont egalement retiree du code. Cette boucle ne se retrouve
-    // donc pas dans le code compile et ne devrait pas etre analysee.
-    //
-    // Pour les utilisateurs de Visual Studio, la compilation en mode
-    // "release" est ce qui s'approche le plus de l'option -O3 avec g++
-    assert(i->size() == budget + 1);
-  }
+  // for (std::vector<std::vector<unsigned int> >::const_iterator i = profits.begin(); i != profits.end(); i++) {
+  //   // Note: Lorsque l'on compile avec l'option -O3, les assertions ne
+  //   // sont pas incluses dans le code compile. Les boucles qui ne font
+  //   // rien sont egalement retiree du code. Cette boucle ne se retrouve
+  //   // donc pas dans le code compile et ne devrait pas etre analysee.
+  //   //
+  //   // Pour les utilisateurs de Visual Studio, la compilation en mode
+  //   // "release" est ce qui s'approche le plus de l'option -O3 avec g++
+  //   assert(i->size() == budget + 1);
+  // }
+  
 
-  // Inserez votre code ici
 }
