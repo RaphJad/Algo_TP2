c8976183881f2c58d66543241bd0c7f0b9c59a41
RaphJad <21295@ecam.be>
2022-12-11 12:11:30 -0500

    Test investissement

--- question3/investissement.cpp
+++ question3/investissement.cpp
@@ -5,6 +5,7 @@
 
 #include <assert.h>
 #include <numeric>
+#include <iostream>
 
 // Parametres:
 //   profits: profits[i][m] = le profit d'investir m dollars dans l'entreprise i
@@ -66,6 +67,7 @@ void investissement(const std::vector<std::vector<unsigned int> >& profits, std:
     if(budget >= profits[0].size()){
       //cas 1 le budget est supérieur au montant maximal à investir
       std::vector<unsigned int> profit_max = find_max_profit(updated_profits, -1);
+      std::cout << profit_max[0] << " ," << profit_max[1] << ", " << profit_max[2] < std::endl;
       //on indique cet investissement dans le tableau des montants
       montants[profit_max[1]] = profit_max[0];
       //on update le budget
