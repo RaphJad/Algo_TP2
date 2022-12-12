26d0fe00506f942b74fe43c665a95c225a09c228
RaphJad <21295@ecam.be>
2022-12-11 20:40:12 -0500

    Add new question 3 from scratch DEBUG 18

--- q3/investissement.cpp
+++ q3/investissement.cpp
@@ -77,9 +77,9 @@ void investissement(const std::vector<std::vector<unsigned int> >& profits, std:
           -> on met à 1 la première case de la ligne dans laquelle on 
              vient de trouver le max    
     */
-   std::cout << max[1] << ", "<< new_budget << std::endl;
-   std::vector<std::vector<unsigned int>> new_profits = update_profits(profits, max[1], new_budget);
-   //on appelle recursivement sur le nouveau tableau de profits trouvé
-   investissement(new_profits, montants);
+    std::cout << max[0] << ", " << max[1] << ", " << max[2] << ", " << new_budget << std::endl;
+    std::vector<std::vector<unsigned int>> new_profits = update_profits(profits, max[1], new_budget);
+    //on appelle recursivement sur le nouveau tableau de profits trouvé
+    investissement(new_profits, montants);
   }
 }
