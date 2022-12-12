dfb7db2ecee9ba26317d97664c70b742aa9429a4
RaphJad <21295@ecam.be>
2022-12-11 20:46:36 -0500

    Add new question 3 from scratch DEBUG 20

--- q3/investissement.cpp
+++ q3/investissement.cpp
@@ -42,6 +42,7 @@ std::vector<std::vector<unsigned int>> update_profits(const std::vector<std::vec
       temp.push_back(0);
     }
     updated_profits.push_back(temp);
+    std::cout << updated_profits[i][0] << std::endl;
   }
   updated_profits[company][0] = 1;
   return updated_profits;
